baacb786ef09e25d362df4f7c3d0e7ba22480ca2
nouryy17 <dacinour16@gmail.com>
2024-07-13 11:43:44 +0200

    Amendment procedure

--- The project/js/script.js
+++ The project/js/script.js
@@ -12,23 +12,34 @@ function toggleActivClass(clickEle) {
 
 }
 
-
-// let newURL = document.getElementById("shorturl");
-// let coptButton = document.getElementById("copy");
-
-// coptButton.onclick = ()=>{
-//     newURL.select();
-
-//     window.navigator.clipboard.writeText(newURL.value);
-// }
-
 const shortenButten=document.querySelector(".from form button.shorten")
-const inputEmail=document.querySelector(".form form input")
+const inputEmail=document.getElementById("longurl")
 const boxShortEmail=document.querySelector(".from from .short_links")
 
 
-shortenButten.addEventListener("click",function () {
-if (inputEmail.value="") {
-     console.log("inputEmail.value")
+shortenButten.addEventListener("click",function (ele) {
+    ele.preventDefault()
+if (inputEmail.value==="") {
+console.log(inputEmail)
+    inputEmail.style.outline="2px solid red"
+}
+if (inputEmail.value!=="") {
+    console.log("ddddddddddddddddddddddd")
+    createDivShortEmial(inputEmail.value)
 }
 })
+function createDivShortEmial(email) {
+    email.style.outline="none"
+    let div=document.createElement("div")
+    let p=document.createElement("p")
+    let p2=document.createElement("p")
+    let span=document.createElement("span")
+    boxShortEmail.appendChild(div)
+    div.appendChild(p)
+    div.appendChild(p2)
+    div.appendChild(span)
+    p2.classList.add("tow")
+    p.textContent(email)
+    p2.textContent(email)
+    span.textContent("Copy")  
+}
